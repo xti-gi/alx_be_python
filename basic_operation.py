9bc678762f5fd36bc6e1e472d6aee395149cab0f
Alex Gathecha <axferguson2@gmail.com>
2025-07-26 06:53:54 +0300

    Add files via upload

---
+++ basic_operation.py
@@ -0,0 +1,10 @@
+number1 = 12
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 + number2
+multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is: {addition}")
+print(f"Subtraction of {number2} from {number1} is: {subtraction}")
+print(f"Multiplication of {number1} and {number2} is: {multiplication}")
